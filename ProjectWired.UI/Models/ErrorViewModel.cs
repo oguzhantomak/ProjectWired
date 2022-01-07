3c27fb21d8d3feff6c99fd56eeaf65a5538bcdf8
oguzhantomak <oguzhantomak@gmail.com>
2022-01-07 16:06:48 +0300

    Add project files.

---
+++ ProjectWired.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectWired.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
